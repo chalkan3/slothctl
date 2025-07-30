9c9c3e0fd6187afe4078620fe392e79372344995
igor.rodrigues <igor.rodrigues@placeti.com.br>
2025-07-30 17:39:07 -0300

    feat(vpn): Add flags to create-config for customization

--- pkg/commands/vpn/create_config.go
+++ pkg/commands/vpn/create_config.go
@@ -19,21 +19,27 @@ func (c *createConfigCmd) Parent() string {
 func (c *createConfigCmd) CobraCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "create-config [filename]",
-		Short: "Creates a sample openfortivpn configuration file",
-		Long:  `Generates a new configuration file with a standard template for openfortivpn.`,
+		Short: "Creates a customizable openfortivpn configuration file",
+		Long:  `Generates a new configuration file for openfortivpn, allowing customization via flags.`,
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			filename := args[0]
+
+			host, _ := cmd.Flags().GetString("host")
+			port, _ := cmd.Flags().GetInt("port")
+			user, _ := cmd.Flags().GetString("user")
+			cert, _ := cmd.Flags().GetString("cert")
+
 			log.Info("Creating new VPN configuration file...", "file", filename)
 
-			configContent := `### configuration file for openfortivpn, see man openfortivpn(1) ###
+			configContent := fmt.Sprintf(`### configuration file for openfortivpn, see man openfortivpn(1) ###
 #
-trusted-cert = 9a820421292fb9a74a3b60f502532d2eb5bb539b7dd13046e912522d6ffde185
-port = 10443
-username = igor.rodrigues
-host = 201.48.63.233
+host = %s
+port = %d
+username = %s
+trusted-cert = %s
 disallow-invalid-cert = 1
-`
+`, host, port, user, cert)
 
 			if err := os.WriteFile(filename, []byte(configContent), 0644); err != nil {
 				log.Error("Failed to write configuration file", "error", err)
@@ -44,6 +50,13 @@ disallow-invalid-cert = 1
 			return nil
 		},
 	}
+
+	// Add flags for customization
+	cmd.Flags().String("host", "201.48.63.233", "VPN gateway host")
+	cmd.Flags().Int("port", 10443, "VPN gateway port")
+	cmd.Flags().String("user", "igor.rodrigues", "VPN username")
+	cmd.Flags().String("cert", "9a820421292fb9a74a3b60f502532d2eb5bb539b7dd13046e912522d6ffde185", "Trusted certificate hash")
+
 	return cmd
 }
 
