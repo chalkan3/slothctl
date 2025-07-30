77ea7e6ecfd8d23604e96ee3a245cd6c5541dc83
igor.rodrigues <igor.rodrigues@placeti.com.br>
2025-07-30 16:39:06 -0300

    fix: update install.sh to point to correct install_slothctl_full.sh commit

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # URL to the full installation script, using a specific commit hash for immutability
-FULL_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/chalkan3/slothctl/50da7ae4250bce55ebbfd7ce8a81c73abe620a26/hacks/install_slothctl_full.sh"
+FULL_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/chalkan3/slothctl/a8666bfba274ac55d6047683d211862348c58c11/hacks/install_slothctl_full.sh"
 
 echo "Downloading and executing the slothctl installer..."
 
